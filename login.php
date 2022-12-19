38fb3565e6f442aba9e6a5d2aa881e8f05b805dc
PaulBeaussart33 <login02400@gmail.com>
2022-12-19 15:23:22 +0100

    19/12/2022

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foodster</title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+<body>
+<form>
+<form>
+    <label for="name">Nom :</label>
+    <input type="text" id="name" name="name">
+    <label for="email">Adresse e-mail :</label>
+    <input type="email" id="email" name="email">
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password">
+</body>
+</html>
